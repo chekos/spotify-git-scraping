31b8326a20e451c9049c65cb6835f25f71034099
sergio <sergio@cimarron.io>
2022-08-09 14:22:03 -0700

    handle env vars and add api constants

--- constants.py
+++ constants.py
@@ -2,7 +2,11 @@ from dotenv import dotenv_values
 import os
 import base64
 import urllib.parse
+from pathlib import Path
 
+PARENT_FOLDER = Path(__file__).parent
+AUTH_FILE = PARENT_FOLDER.joinpath("auth.json")
+TOKEN_FILE = PARENT_FOLDER.joinpath("token_info.json")
 config = {**dotenv_values(), **os.environ}
 
 CLIENT_ID = config["SPOTIFY_CLIENT_ID"]
@@ -10,9 +14,10 @@ CLIENT_SECRET = config["SPOTIFY_CLIENT_SECRET"]
 REDIRECT_URI = config["REDIRECT_URI"]
 USERNAME = config["USERNAME"]
 PASSWORD = config["PASSWORD"]
-SCOPE = "user-read-recently-played"
+SCOPE = "user-read-recently-played,user-top-read"
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com/v1"
 
 AUTH_STRING = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode("utf-8")).decode(
     "utf-8"
