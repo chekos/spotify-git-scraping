9c69404cdffe3ff965d31739577d9efc20cb07b8
sergio <sergio@cimarron.io>
2022-08-10 09:28:22 -0700

    simplify auth workflow

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 from dotenv import dotenv_values
 import os
-import base64
 import urllib.parse
 from pathlib import Path
 from typer import get_app_dir
@@ -14,7 +13,6 @@ TOKEN_FILE = APP_DIR.joinpath("token_info.json")
 config = {**dotenv_values(), **os.environ}
 
 CLIENT_ID = config["SPOTIFY_CLIENT_ID"]
-CLIENT_SECRET = config["SPOTIFY_CLIENT_SECRET"]
 REDIRECT_URI = config["REDIRECT_URI"]
 USERNAME = config["USERNAME"]
 PASSWORD = config["PASSWORD"]
@@ -23,14 +21,11 @@ SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_API_BASE_URL = "https://api.spotify.com/v1"
 
-AUTH_STRING = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode("utf-8")).decode(
-    "utf-8"
-)
+AUTH_STRING = config["SPOTIFY_AUTH_STRING"]
 
 
 access_code_params = {
     "client_id": CLIENT_ID,
-    "client_secret": CLIENT_SECRET,
     "redirect_uri": REDIRECT_URI,
     "scope": SCOPE,
     "response_type": "code",
