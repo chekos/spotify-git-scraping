11a84e3bb01b2653241d7f25d7e169e1d45f358a
sergio <sergio@cimarron.io>
2022-08-10 10:51:56 -0700

    drop examples from repo

--- examples/playwright/tutorial.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-from rich import print
-
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("http://whatsmyuseragent.org/")
-    print(page.title())
-    browser.close()
